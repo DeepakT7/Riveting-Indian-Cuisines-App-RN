af2b21d95d72a1d214e606268becc81890b2b0f9
Deepak Tejwani <deepaktejwani1@gmail.com>
2020-05-22 18:01:14 +0530

    Create ContactMe.js

---
+++ screens/ContactMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/Colors';
+
+const ContactMe = props => {
+    return(
+<Text>Hey!</Text>
+    )
+}
+
+
+export default ContactMe;
